6c3e821743b15da8dc157eb36fd22a50b332757d
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 11:44:07 +0100

    feat: add others emoji

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,8 +1,8 @@
 // 1. L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore
 
-const num1 = Number(prompt("Inserisci il primo numero"))
-const num2 = Number(prompt("Inserisci il secondo numero"))
+const num1 = Number(prompt("Inserisci il primo numero ⬇"))
+const num2 = Number(prompt("Inserisci il secondo numero ⬇"))
 
 if (num1 > num2) {
     alert (`🔝 Il primo numero inserito è maggiore del secondo 🚀`)
