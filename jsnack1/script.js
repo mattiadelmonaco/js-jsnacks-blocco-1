ce6fd33a64b5b30142394eac585cecec72420016
mattiadelmonaco <mattia.delmonaco@gmail.com>
2024-12-03 11:42:58 +0100

    feat: add ugual number and emoji

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,11 +1,13 @@
-// L’utente inserisce due numeri in successione, con due prompt.
+// 1. L’utente inserisce due numeri in successione, con due prompt.
 // Il software stampa il maggiore
 
 const num1 = Number(prompt("Inserisci il primo numero"))
 const num2 = Number(prompt("Inserisci il secondo numero"))
 
 if (num1 > num2) {
-    alert (`Il primo numero inserito è maggiore del secondo`)
+    alert (`🔝 Il primo numero inserito è maggiore del secondo 🚀`)
+} else if (num2 > num1) {
+    alert ("🔝 Il secondo numero è maggiore del primo 🚀")
 } else {
-    alert ("Il secondo numero è maggiore del primo")
+    alert ("♊ I numeri sono uguali ♊")
 }
